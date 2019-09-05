448427e16e08d7e7b419ccfb933330cf179dcfa4
ecoult <steven.xie@ecoult.com>
2019-09-05 11:50:07 +1000

    staged change

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,13 @@
+console.log("Hello World!");
+
+var firstname = "Gawain";
+var lastname = "XIE";
+var age = 30;
+console.log("Firstname %s, Lastname %s, Age %d.", firstname, lastname, age);
+
+//undefined
+var job;
+console.log(job);
+
+job = "Engineer";
+console.log(job);
